520fc6d081cde125d6561df1a8cce735ba2b7ecc
Femi Adegoke <adegokefemi17@gmail.com>
2023-08-16 21:58:36 +0100

    Add page_view_indicator (#1)
    * Add page_view_indicator
    
    * Fix issue with indicators not being initialized

---
+++ packages/page_view_indicator/example/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:page_view_indicator_ns/page_view_indicator_ns.dart';
+
+class App extends StatelessWidget {
+  static const length = 3;
+  final pageIndexNotifier = ValueNotifier<int>(0);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          alignment: FractionalOffset.bottomCenter,
+          children: <Widget>[
+            PageView.builder(
+              onPageChanged: (index) => pageIndexNotifier.value = index,
+              itemCount: length,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(color: Colors.accents[index]),
+                );
+              },
+            ),
+            _buildExample1()
+          ],
+        ),
+      ),
+    );
+  }
+
+  PageViewIndicator _buildExample1() {
+    return PageViewIndicator(
+      pageIndexNotifier: pageIndexNotifier,
+      length: length,
+      normalBuilder: (animationController, index) => Circle(
+        size: 8.0,
+        color: Colors.black87,
+      ),
+      highlightedBuilder: (animationController, index) => ScaleTransition(
+        scale: CurvedAnimation(
+          parent: animationController,
+          curve: Curves.ease,
+        ),
+        child: Circle(
+          size: 12.0,
+          color: Colors.accents.elementAt((index + 3) * 3),
+        ),
+      ),
+    );
+  }
+}
+
+void main() => runApp(App());
---
+++ packages/page_view_indicator/lib/src/indicator.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+/// Mediates indicator building and animation.
+class Indicator {
+  /// [normalController] Animation controller when state changes to normal
+  /// [highlightedController] Animation controller when state changes to selected
+  /// [widget] Indicator widget
+  /// [builder] Widgetbuilder object
+  /// [index] Determine index on the stack
+  Indicator({
+    required this.normalController,
+    required this.highlightedController,
+    this.widget,
+    this.builder,
+    required this.index,
+  });
+
+  final AnimationController normalController;
+  final AnimationController highlightedController;
+  final Widget? widget;
+  final WidgetBuilder? builder;
+  final int index;
+
+  void dispose() {
+    normalController.dispose();
+    highlightedController.dispose();
+  }
+}
