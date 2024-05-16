a457bbfa7b82bca8c0f712171a10667d17f0ea77
Slava R <viacheslav.riabinin@headspace.com>
2024-05-16 17:54:34 +0300

    [MSE-130] Add more tests and helper tool to simplify testing of widgets containing AdaWebView (#10)
    * Add tests helper and test for AdaWebView
    
    * Version 0.0.2
    
    * Export mock_web_view_dependencies helper
    
    * Move fk_user_agent dependency

--- packages/ada_chat_flutter/CHANGELOG.md
+++ packages/ada_chat_flutter/CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.0.2
+
+- Add tests helper.
+- Add test for AdaWebView.
+
 ## 0.0.1
 
 Initial release.
--- packages/ada_chat_flutter/lib/ada_chat_flutter.dart
+++ packages/ada_chat_flutter/lib/ada_chat_flutter.dart
@@ -3,3 +3,4 @@ library ada_chat_flutter;
 export 'src/ada_controller.dart';
 export 'src/ada_exception.dart';
 export 'src/ada_web_view.dart';
+export 'src/test/mock_web_view_dependencies.dart';
